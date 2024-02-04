0edd695e7dddae23d840e3bf443423de62749a46
exzolink <exzolink@gmail.com>
2024-02-04 04:49:41 +0300

    Update calculator.php

--- calculator.php
+++ calculator.php
@@ -26,25 +26,25 @@ if (empty($worth)) {
 
 $request = new Requests\CalculationWithTariffListAuthorizedRequest();
 $request
-		->setSenderCityId($from)
-		->setReceiverCityId($to)
-		->addAdditionalService(2, $worth)
+	->setSenderCityId($from)
+	->setReceiverCityId($to)
+	->addAdditionalService(2, $worth)
 
-		->addTariffToList(139) // ИМ Д-Д
-		->addTariffToList(137) // ИМ С-Д
-		->addTariffToList(136) // ИМ С-С
-		->addTariffToList(1) // Лайт Д-Д
-		->addTariffToList(11) // Лайт С-Д
-		->addTariffToList(10) // Лайт С-С
-		->addTariffToList(62) // Маг. Экспресс С-С
-		->addTariffToList(5) // Эконом экспр. С-С
+	->addTariffToList(139) // ИМ Д-Д
+	->addTariffToList(137) // ИМ С-Д
+	->addTariffToList(136) // ИМ С-С
+	->addTariffToList(1) // Лайт Д-Д
+	->addTariffToList(11) // Лайт С-Д
+	->addTariffToList(10) // Лайт С-С
+	->addTariffToList(62) // Маг. Экспресс С-С
+	->addTariffToList(5) // Эконом экспр. С-С
 
-		->addPackage([
-				'weight' => $weight,
-				'length' => $dlength,
-				'width'  => $width,
-				'height' => $height,
-		]);
+	->addPackage([
+		'weight' => $weight,
+		'length' => $dlength,
+		'width'  => $width,
+		'height' => $height,
+	]);
 
 $response = $client->sendCalculationWithTariffListRequest($request);
 
@@ -52,38 +52,38 @@ $data = [];
 
 foreach ($response->getResults() as $result) {
 
-		if ($result->hasErrors()) {
-				$key = "id_" . $result->getTariffId();
-				$value = false;
-				$data[$key] = $value;
-				continue;
-		}
+	if ($result->hasErrors()) {
+		$key = "id_" . $result->getTariffId();
+		$value = false;
+		$data[$key] = $value;
+		continue;
+	}
 
-		if (!$result->getStatus()) {
-				continue;
-		}
+	if (!$result->getStatus()) {
+		continue;
+	}
 
 	if ($cod && intval($worth) > 0) {
-			$sum = intval($worth) + intval($result->getPrice());
-			$percent = ($sum * 3) / 100;
-			$calcPrice = $sum + $percent + 1;
+		$sum = intval($worth) + intval($result->getPrice());
+		$percent = ($sum * 3) / 100;
+		$calcPrice = $sum + $percent + 1;
 
-			$key = "id_" . $result->getTariffId();
-			$value = $calcPrice;
-			$data[$key] = $value;
-		} else {
-			$key = "id_" . $result->getTariffId();
-			$value = $result->getPrice();
-			$data[$key] = $value;
-		}
-
-		$key = "min_id_" . $result->getTariffId();
-		$value = $result->getDeliveryPeriodMin();
+		$key = "id_" . $result->getTariffId();
+		$value = $calcPrice;
 		$data[$key] = $value;
-
-		$key = "max_id_" . $result->getTariffId();
-		$value = $result->getDeliveryPeriodMax();
+	} else {
+		$key = "id_" . $result->getTariffId();
+		$value = $result->getPrice();
 		$data[$key] = $value;
+	}
+
+	$key = "min_id_" . $result->getTariffId();
+	$value = $result->getDeliveryPeriodMin();
+	$data[$key] = $value;
+
+	$key = "max_id_" . $result->getTariffId();
+	$value = $result->getDeliveryPeriodMax();
+	$data[$key] = $value;
 }
 
 echo json_encode($data);
